d341dc3688c2c88b0164d0cb15471e6f6e0235a8
Mark Entingh <entingh@easci.com>
2016-04-26 15:03:42 -0700

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Renamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataFiles = new System.Windows.Forms.DataGridView();
+            this.Filename = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Renamed = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataFiles
+            // 
+            this.dataFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Filename,
+            this.Renamed});
+            this.dataFiles.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataFiles.Location = new System.Drawing.Point(0, 54);
+            this.dataFiles.Name = "dataFiles";
+            this.dataFiles.Size = new System.Drawing.Size(782, 410);
+            this.dataFiles.TabIndex = 1;
+            // 
+            // Filename
+            // 
+            this.Filename.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Filename.DataPropertyName = "name";
+            this.Filename.HeaderText = "Filename";
+            this.Filename.Name = "Filename";
+            this.Filename.Width = 74;
+            // 
+            // Renamed
+            // 
+            this.Renamed.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Renamed.DataPropertyName = "renamed";
+            this.Renamed.HeaderText = "Renamed";
+            this.Renamed.Name = "Renamed";
+            this.Renamed.Width = 78;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 464);
+            this.Controls.Add(this.dataFiles);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataFiles)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataFiles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Filename;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Renamed;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Renamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var path = "C:\\Users\\Mark\\Desktop\\TriblerDownloads\\One_Piece_1-516\\";
+            var extensions = new string[] { ".avi", ".mp4", ".mkv" };
+            var files = GetFiles(path, extensions.ToList());
+            string[] p;
+            FileDetails file;
+            var details = new List<FileDetails>();
+            List<FileDetails> results;
+
+            foreach (string f in files)
+            {
+                p = f.ToLower().Split('\\');
+                file = new FileDetails(p[p.Length - 1]);
+                p = file.name.Split('.');
+                file.extension = p[p.Length - 1];
+                file.episode = GetEpisodeNumber(file.name);
+                file.renamed = "One Piece - Ep " + file.episode.ToString("000") + "." + file.extension;
+                details.Add(file);
+
+                try
+                {
+                    File.Move(path + file.name, path + file.renamed);
+                }catch(Exception ex) { }
+            }
+            results = details.OrderBy(o => o.episode).ToList();
+            dataFiles.AutoGenerateColumns = false;
+            dataFiles.DataSource = results;
+        }
+
+        private int GetEpisodeNumber(string str)
+        {
+            var i = -1;
+            var o = 0;
+            var s = "";
+            var result = 0;
+            for (var x = 0; x < str.Length; x++)
+            {
+                s = str.Substring(x, 1);
+                if (int.TryParse(s, out o) == true)
+                {
+                    if (i == -1)
+                    {
+                        //found beginning of number
+                        i = x;
+                    }
+                }
+                else
+                {
+                    if (i >= 0)
+                    {
+                        //found end of number
+                        o = int.Parse(str.Substring(i, x - i));
+                        if (o < 1 || o > 516)
+                        {
+                            //not a believable episode number
+                            i = -1;
+                        }
+                        else
+                        {
+                            //found episode number, exit
+                            result = o;
+                            break;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        private List<string> GetFiles(string folder, List<string> extensions)
+        {
+            List<string> files = new List<string>();
+            try
+            {
+                foreach (string f in Directory.GetFiles(folder).Where(d => extensions.Count(e => d.IndexOf(e) > 0) > 0).ToList())
+                {
+                    files.Add(f);
+                }
+                foreach (string d in Directory.GetDirectories(folder))
+                {
+                    files.AddRange(GetFiles(d, extensions));
+                }
+            }
+            catch (Exception excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+
+            return files;
+        }
+    }
+
+    public class FileDetails
+    {
+        public string name { get; set; }
+        public string renamed { get; set; } 
+        public string extension { get; set; }
+        public int episode { get; set; }
+
+        public FileDetails(string name = "", string renamed = "", string extension = "")
+        {
+            this.name = name;
+            this.renamed = renamed;
+            this.extension = extension;
+            this.episode = 0;
+        }
+    }
+}
